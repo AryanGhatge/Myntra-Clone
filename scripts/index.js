8b1693b7d3c30819017b38f16723875ef274fb3d
AryanGhatge <aughatge007@gmail.com>
2024-04-13 02:33:58 +0530

    Saturday1

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,51 @@
-let itemContainerElement = document.querySelector('.items-container');
-let innerHTML = '';
+let bagItems = [];
+onLoad();
+function onLoad(){
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+    displayItemsOnHomePage();
+    displayBagIcon();
+}
 
-items.forEach(item => {
-    innerHTML += `
-    <div class="item-container">
-        <img class="item-image" src="${item.item_image}" alt="Item-Image">
-        <div class="rating">${item.rating.stars}⭐ | ${item.rating.noOfReview}</div>
-        <div class="company-name">${item.company_name}</div>
-        <div class="item-name">${item.item_name}</div>
-        <div class="pricing">
-            <span class="current-price">${item.current_price}</span>
-            <span class="original-price">${item.original_price}</span>
-            <span class="discount">${item.discount}% OFF</span>
+function addToBag(itemId) {
+    bagItems.push(itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    displayBagIcon();
+};
+
+function displayBagIcon(){
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if(bagItems.length > 0){
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerText = bagItems.length;
+    }
+    else{
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+}
+
+function displayItemsOnHomePage(){
+    let itemContainerElement = document.querySelector('.items-container');
+    if(!itemContainerElement) return;
+    let innerHTML = '';
+
+    items.forEach(item => {
+        innerHTML += `
+        <div class="item-container">
+            <img class="item-image" src="${item.image}" alt="Item-Image">
+            <div class="rating">${item.rating.stars}⭐ | ${item.rating.count}</div>
+            <div class="company-name">${item.company}</div>
+            <div class="item-name">${item.item_name}</div>
+            <div class="pricing">
+                <span class="current-price">${item.current_price}</span>
+                <span class="original-price">${item.original_price}</span>
+                <span class="discount">${item.discount}% OFF</span>
+            </div>
+            <button class="btn-add-bag" onclick = "addToBag(${item.id})">Add to Bag</button>
         </div>
-        <button class="btn-add-bag">Add to Bag</button>
-    </div>
-    `;
-});
+        `;
+    });
 
-itemContainerElement.innerHTML = innerHTML;
+    itemContainerElement.innerHTML = innerHTML;
+};
+
